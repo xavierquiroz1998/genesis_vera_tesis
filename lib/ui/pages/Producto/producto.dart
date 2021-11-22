55bc9a78b6a9b2bbf7c65a8d0460f5a5653fcbbb
xavier Quiroz <xavierpolivioquirozbriones@hotmail.com>
2021-11-21 23:12:57 -0500

    creacion de pantall producto crud

---
+++ lib/ui/pages/Producto/producto.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductoCrud extends StatefulWidget {
+  ProductoCrud({Key? key}) : super(key: key);
+
+  @override
+  _ProductoCrudState createState() => _ProductoCrudState();
+}
+
+class _ProductoCrudState extends State<ProductoCrud> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
